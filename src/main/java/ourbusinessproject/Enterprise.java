79583837db3ff82874bd9626ec30af1b50864791
Spleedz <maxence.beth@etu.iut-tlse3.fr>
2026-01-27 10:48:53 +0100

    fix #1.2 classe Enterprise

---
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -0,0 +1,54 @@
+package ourbusinessproject;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class Enterprise {
+    @NotBlank
+    private String name;
+
+    @Size(min = 10)
+    private String description;
+
+    @NotBlank
+    private String contactName;
+
+    @NotBlank
+    @Pattern(regexp="^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
+    private String contactEmail;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+}
